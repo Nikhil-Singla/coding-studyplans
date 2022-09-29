885227cbc7752fa58fc76b1b0dad7151e9373b04
Nikhil-Singla <55959861+Nikhil-Singla@users.noreply.github.com>
2022-09-29 14:55:18 -0500

    Create binarysearch.cpp
    First code that passed the test cases. Error at a single list test case.

---
+++ algorithm_1/day1_binarysearch/binarysearch.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int start = 0;
+        int end = nums.size()-1;
+        int mid = 0;
+        mid = ((start)+end)/2;
+        while(start<end)
+        {
+            if(nums.at(mid) == target)
+                return mid;
+            if(nums.at(mid) < target)
+            {
+                start = mid;
+            }
+            if(nums.at(mid) > target)
+            {
+                end = mid;
+            }
+            mid = ((start++)+end)/2;
+        }
+        return (-1);
+    }
+};
