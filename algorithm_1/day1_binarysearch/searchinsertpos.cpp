14074146c05fa848a55ac76828bd6b0be5b01aff
Nikhil-Singla <55959861+Nikhil-Singla@users.noreply.github.com>
2022-09-29 17:00:50 -0500

    Update searchinsertpos.cpp
    Tried to tackle the problem from a new angle and from the beginning to apply everything that has been learnt in one go.

--- algorithm_1/day1_binarysearch/searchinsertpos.cpp
+++ algorithm_1/day1_binarysearch/searchinsertpos.cpp
@@ -2,30 +2,16 @@ class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) 
     {
-        int low = 0, high = nums.size()-1, mid, answer;
-        while(low<=high)
+        int low = 0, high = nums.size()-1;
+        int mid=0;
+        for(mid = (low+high)/2; low<=high; mid = (low+high)/2)
         {
-            mid = (low+high)/2;
             if(nums[mid]==target)
-            {    
-                answer = mid;
-                break;
-            }
-            else if(nums[mid]<target)
             {
-                low = mid+1;
-                answer = low;
+                return mid;
             }
-            else
-            {
-                answer = high;
-                high = mid-1;
-            }
+            (nums[mid]>target ? high=mid-1:low=mid+1);
         }
-        if(answer<0)
-        {
-            answer = 0;
-        }
-        return answer;
+        return mid;
     }
 };
