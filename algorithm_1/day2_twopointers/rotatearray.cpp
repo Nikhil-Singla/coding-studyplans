6ee05ec9099ee703fb46499f7e3f24494d813449
Nikhil-Singla <55959861+Nikhil-Singla@users.noreply.github.com>
2022-10-01 02:25:09 -0500

    Update rotatearray.cpp
    Lost the version somewhere in translation while bouncing between leetcode problems sharing a single theoretical concept.

--- algorithm_1/day2_twopointers/rotatearray.cpp
+++ algorithm_1/day2_twopointers/rotatearray.cpp
@@ -1,80 +1,31 @@
-# include <string>
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-        vector<int> hash(256, -1);
-        int length = s.length(), result=0, var=0;
-        if(length<2)
+    void rotate(vector<int>& nums, int k) {
+        int a = nums.size(), l=0;
+        vector<int> rot(a);
+        int flag = 0, flag2 = 0;
+        for(int i=0,j=0; i<a && j<k; i++)
         {
-            return length;
-        }
-        else if(length == 2)
-        {
-            if(s.at(0)==s.at(1))
-            {return(1);}
+            flag2 = 1;
+            l = i+k;
+            if(l<a)
+                {rot[l]=nums[i];}
             else
-            {return 2;}
-        }
-        auto first=s.begin(), second=s.end();
-        auto temp = first+1;
-        int ascii=0;
-        ascii = int(*first);
-        hash[ascii] = 1;
-        length = 1;
-        while(temp<=second)
-        {
-            while(*temp!=*first && temp<=second)
             {
-                ascii = int(*temp);
-                if(hash[ascii]==1)
+                if(flag == 0)
                 {
-                    if(result<var)
-                    {
-                        result = var;
-                    }
-                    var = 1;
-                    first = temp++;
+                    l=0;
+                    rot[l]=nums[i];
+                    flag = 1;
+                    j++;
                 }
-                hash[ascii] = 1;
-                var++;
+                else
+                {
+                    rot[j]=nums[i];
+                    j++;
+                }
             }
-            var = 1;
-            first = temp++;
         }
-        return result;
+        if(flag2==1){nums = rot;}
     }
 };
-
-//OLD Code for experience
-        /*int len = 0, size=s.size();
-        string::iterator first, second, last;
-        first = s.begin();
-        second = first+1;
-        last = s.end()-2;
-        int temp = 0;
-        int furst = -1, seconduh = -1;
-        while(second <= last)
-        {
-            temp = 1;
-            // std::cout<<*first;
-            vector<pair<int,int>> hash_set(256, -1);
-            furst = *first;
-            seconduh = *second;
-            hash_set[furst] = 1;
-            hash_set[seconduh] = 1;
-            while((hash_set[*second]!=1) && (second<=last))
-            {
-                hash_set[*second] = 1;
-                ++temp;
-                ++second;
-                std::cout<<*second;
-            }
-            ++second;
-            // std::cout<<*second;
-            first = second;
-            if(len<temp)
-            {
-                len = temp;
-            }
-        }
-        return len;*/
