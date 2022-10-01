3145f48d0a3d4118b5bedf3cf6e600d316b72ace
Nikhil-Singla <55959861+Nikhil-Singla@users.noreply.github.com>
2022-09-30 22:04:21 -0500

    Update squareofsort.cpp
    Code was accepted on the first try

--- algorithm_1/day2_twopointers/squareofsort.cpp
+++ algorithm_1/day2_twopointers/squareofsort.cpp
@@ -2,12 +2,12 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        int a = nums.size();
-        for(int i=0;i<a;i++)
+        int a = nums.size(); //store nums.size separately
+        for(int i=0;i<a;i++) //run it the amount of times needed till it crosses size, ie from start to finish
         {
-            nums[i] *= nums[i];
+            nums[i] *= nums[i]; //square each of the nums[i] terms
         }
-        sort(nums.begin(),nums.end());
+        sort(nums.begin(),nums.end()); //simple sorting function
         return nums;
     }
 };
