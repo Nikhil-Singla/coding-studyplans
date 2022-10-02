fc722f652cd11d151220c4805897c732e56f8800
Nikhil-Singla <55959861+Nikhil-Singla@users.noreply.github.com>
2022-10-01 23:09:35 -0500

    Update reversestring.cpp
    First solution was too easy. Tried the next one. But before I could test run the code, the website went down. This is what I have written so far.

--- algorithm_1/day4_twopointers/reversestring.cpp
+++ algorithm_1/day4_twopointers/reversestring.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        reverse(s.begin(),s.end());
+        //reverse(s.begin(),s.end());
+        int i = s.size(), last=s.size()-1;
+        if(i%2==0)
+        {
+            i /= 2;
+        }
+        else
+        {
+            --i /=2;
+        }
+        for(int j=0; j<i; j++)
+        {
+            swap(s[j],s[last--]);
+        }
     }
 };
