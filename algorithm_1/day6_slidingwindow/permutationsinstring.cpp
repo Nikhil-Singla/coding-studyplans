c3165a9b69e6be14b64465a0a3e2723709d366cc
Nikhil-Singla <55959861+Nikhil-Singla@users.noreply.github.com>
2022-10-03 22:10:51 -0500

    Update permutationsinstring.cpp
    First code that worked successfully for the given testcases.

--- algorithm_1/day6_slidingwindow/permutationsinstring.cpp
+++ algorithm_1/day6_slidingwindow/permutationsinstring.cpp
@@ -4,6 +4,7 @@ public:
     {
         array<int, 26>hash={0};
         array<int, 26>comp={0};
+        //cout<<(hash == comp)<<endl;
         if(s1.length()>s2.length())
         {
             return false;
@@ -13,20 +14,28 @@ public:
         {
             hash[s1[point1]-'a']++;
         }
-
+        //for (int i = 0; i < 26; i++) 
+        //    cout << hash[i];
+        //cout<<endl;
         //Time to compare the hashmap for the substring to the main string
         for(int p2 = 0; p2 < s1.length(); p2++)
         {
             hash[s2[p2]-'a']--;
         }
+        //for (int i = 0; i < 26; i++) 
+        //    cout << hash[i];
+        //cout<<endl;
         if(hash == comp)
         {
             return true;
         }
-        for(int p1 = 0, p2=s1.length()-1; p2<s2.length() ; p1++, p2++)
+        for(int p1 = 0, p2=s1.length(); p2<s2.length() ; p2++)
         {
-            hash[s2[p2]-'a']++;
-            hash[s2[p1]-'a']--;
+            hash[s2[p2]-'a']--;
+            hash[s2[p1++]-'a']++; 
+            //for (int i = 0; i < 26; i++) 
+            //    cout << hash[i];
+            //cout<<endl;
             if(hash == comp)
             {
                 return true;
