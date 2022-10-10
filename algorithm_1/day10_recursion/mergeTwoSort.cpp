09f22a4c52fb9d30a625b0e199d0f45508de933c
Nikhil-Singla <55959861+Nikhil-Singla@users.noreply.github.com>
2022-10-09 21:57:57 -0500

    First try, needs fixing

---
+++ algorithm_1/day10_recursion/mergeTwoSort.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode *result, *temp, *answer;
+        int val = 0;
+        if(list1 == nullptr)
+        {
+            return list2;
+        }
+        if(list2 == nullptr)
+        {
+            return list1;
+        }
+        result = new ListNode;
+        answer = result;
+        while(list1 != nullptr)
+        {
+            if(list1->val > list2->val)
+            {
+                temp = new ListNode(list2->val);
+                result->next = temp;
+                result = result->next;
+                list2 = list2->next;
+            }
+            else
+            {
+                temp = new ListNode(list1->val);
+                result->next = temp;
+                result = result->next;
+                list1 = list1->next;
+            }
+            if(list2 == nullptr)
+            {
+                result->next = list1;
+                break;
+            }
+        }
+        while(list2 != nullptr)
+        {
+            temp = new ListNode(list2->val);
+            result->next = temp;
+            list2 = list2->next;
+        }
+        return answer;
+    }
+};
